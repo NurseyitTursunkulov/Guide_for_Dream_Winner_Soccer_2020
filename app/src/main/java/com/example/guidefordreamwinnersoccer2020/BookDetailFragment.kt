cb6f6839f456fef9724aab7c4c5f2a2f9723d6fb
NurseyitTursunkulov <nurseyit.tursunkukulov@gmail.com>
2020-05-19 20:04:52 +0600

    collapsing toolbar added

--- app/src/main/java/com/example/guidefordreamwinnersoccer2020/BookDetailFragment.kt
+++ app/src/main/java/com/example/guidefordreamwinnersoccer2020/BookDetailFragment.kt
@@ -1,28 +1,27 @@
 package com.example.guidefordreamwinnersoccer2020
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import com.example.guidefordreamwinnersoccer2020.bookList.Book
-import com.example.guidefordreamwinnersoccer2020.databinding.ActivityBookInformationBinding
-import com.example.guidefordreamwinnersoccer2020.databinding.FragmentMainBinding
+import androidx.fragment.app.Fragment
+import com.example.guidefordreamwinnersoccer2020.databinding.FragmentBookDetailBinding
+import kotlinx.android.synthetic.main.fragment_book_detail.*
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class BookDetailFragment : Fragment() {
 
-    lateinit var viewDataBinding: ActivityBookInformationBinding
-    val viewModel:MainViewModel by sharedViewModel()
+    lateinit var viewDataBinding: FragmentBookDetailBinding
+    val viewModel: MainViewModel by sharedViewModel()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         (activity as AppCompatActivity).supportActionBar?.show()
-        viewDataBinding = ActivityBookInformationBinding.inflate(inflater, container, false).apply {
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        viewDataBinding = FragmentBookDetailBinding.inflate(inflater, container, false).apply {
             bookInfo = viewModel.navigateToDetailEvent.value?.peekContent()
         }
         return viewDataBinding.root
