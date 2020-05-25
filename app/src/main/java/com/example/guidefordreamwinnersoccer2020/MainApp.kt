ef999bd56c1430ec86e6eab95bb1437c9ec9637b
NurseyitTursunkulov <nurseyit.tursunkukulov@gmail.com>
2020-05-25 16:55:09 +0600

    yandex added

--- app/src/main/java/com/example/guidefordreamwinnersoccer2020/MainApp.kt
+++ app/src/main/java/com/example/guidefordreamwinnersoccer2020/MainApp.kt
@@ -1,18 +1,30 @@
 package com.example.guidefordreamwinnersoccer2020
 
 import android.app.Application
+import com.yandex.metrica.YandexMetrica
+import com.yandex.metrica.YandexMetricaConfig
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidFileProperties
 import org.koin.android.ext.koin.androidLogger
-import org.koin.androidx.experimental.dsl.viewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
+
 class MainApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        // Creating an extended library configuration.
+        // Creating an extended library configuration.
+        val config =
+            YandexMetricaConfig.newConfigBuilder("91c8db8c-7ee6-4756-aace-0ecb5c8b4c10").build()
+        // Initializing the AppMetrica SDK.
+        // Initializing the AppMetrica SDK.
+        YandexMetrica.activate(applicationContext, config)
+        // Automatic tracking of user activity.
+        // Automatic tracking of user activity.
+        YandexMetrica.enableActivityAutoTracking(this)
         startKoin {
             // use AndroidLogger as Koin Logger - default Level.INFO
             androidLogger()
